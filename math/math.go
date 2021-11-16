aa047e1941f35b6a0e45417daed5fb20e5a5e042
BinXiang123 <bin_shine@me.com>
2021-11-16 10:11:06 +0800

    1111111

--- math/math.go
+++ math/math.go
@@ -1,19 +1,9 @@
 package math
 
-import "fmt"
-
 func Average(nums []float64) float64 {
 	total := float64(0)
 	for _, x := range nums {
 		total += x
 	}
 	return total / float64(len(nums))
-}
-
-func Average2() {
-	fmt.Println("hello")
-}
-
-func ShowName() {
-	fmt.Println("hello")
 }
