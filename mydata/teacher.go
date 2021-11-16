30a0d0ac0c76ed40261389407ef2fc2a986d0e09
BinXiang123 <bin_shine@me.com>
2021-11-16 10:42:13 +0800

    add teacher

---
+++ mydata/teacher.go
@@ -0,0 +1,20 @@
+package mydata
+
+import "fmt"
+
+type Teacher struct {
+	Class int
+	Age   int
+	Name  string
+}
+
+func (t *Teacher) SayHello() {
+	//panic("not implemented") // TODO: Implement
+	fmt.Printf("%s SayHello!", t.Name)
+}
+
+func (t *Teacher) ShowAge() {
+	//panic("not implemented") // TODO: Implement
+	fmt.Printf("%s ShowAge:%d", t.Name, t.Age)
+
+}
